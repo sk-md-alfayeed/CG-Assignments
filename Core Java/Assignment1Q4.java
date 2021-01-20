b2fd62b355527ae5ae1634df6512bc9913f96062
Sk Md Alfayeed <s.m.alfayeed@gmail.com>
2021-01-20 23:04:45 +0530

    Updated Assignment1Q4

--- Core Java/Assignment1Q4.java
+++ Core Java/Assignment1Q4.java
@@ -1,32 +1,39 @@
-class ResultDeclaration{
-    public String declareResults( double subject1Marks, double subject2Marks, double subject3Marks) {
-    	if(subject1Marks > 60) {
-    		if (subject2Marks > 60 && subject3Marks >60) {
-    		return "Passed";
-    		}
-    		else if (subject2Marks > 60 || subject3Marks >60) {
-        		return "Promoted";
-    		}
-    		else if (subject2Marks <= 60 && subject3Marks <= 60) {
-        		return "failed";
-        		}
-    	}
-    	else if(subject1Marks <= 60) {
-    		if (subject2Marks <= 60 || subject3Marks  <= 60) {
-        		return "failed";
-    		}
-    		else if (subject2Marks > 60 && subject3Marks > 60) {
-        		return "Promoted";
-        		}
-    	}
-		return "failed";
-    }
+class ResultDeclaration {
+	public String declareResults(double subject1Marks, double subject2Marks, double subject3Marks) {
+
+		if ((subject1Marks + subject2Marks + subject3Marks) > 60) {
+			if (subject1Marks > 60 && subject2Marks < 60 && subject2Marks < 60) {
+				return "failed";
+			} else if (subject1Marks < 60 && subject2Marks > 60 && subject2Marks < 60) {
+				return "failed";
+			} else if (subject1Marks < 60 && subject2Marks < 60 && subject2Marks > 60) {
+				return "failed";
+			} else if ((subject1Marks + subject2Marks) > 60 || (subject2Marks + subject3Marks) > 60
+					|| (subject3Marks + subject1Marks) > 60) {
+				return "passed"
+						+ "\n"
+						+ "promoted";
+			}
+		}
+
+		if ((subject1Marks + subject2Marks + subject3Marks) < 60) {
+			return "failed";
+		}
+
+		if ((subject1Marks + subject2Marks) > 60 || (subject2Marks + subject3Marks) > 60
+				|| (subject3Marks + subject1Marks) > 60) {
+			return "promoted";
+		}
+		return "passed";
+	}
+
 }
+
 public class Assignment1Q4 {
-    public static void main(String[] args) {
-    	
-    	ResultDeclaration result = new ResultDeclaration();
-    	System.out.println(result.declareResults(10, 20, 40));
-    	
-    }
+	public static void main(String[] args) {
+
+		ResultDeclaration result = new ResultDeclaration();
+		System.out.println(result.declareResults(20, 10, 40));
+
+	}
 }
